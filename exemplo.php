ef5f6eb210d6021bc052e2914ac3c82e3445c87b
Anne G. Rufino Dornelles <140663123+annegdornelles@users.noreply.github.com>
2024-11-05 09:57:46 -0300

    teste de conexao imagem-banco

---
+++ exemplo.php
@@ -0,0 +1,73 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '12345';
+$database = 'cianman';
+
+$mysqli = new mysqli($host, $user, $password, $database);
+
+//Cria uma variável para retornar o array da consulta.
+$result = null;
+
+if (mysqli_connect_errno()) {
+    echo 'Erro ao conectar no banco de dados.';
+} else {
+    echo 'Conexão realizada com sucesso.';
+    //construi minha consulta sql e armazenei na var. $sql.
+    //$sql = 'SELECT * FROM sgp.usuarios where email="m@m" and senha="123"';
+    $img = 'SELECT * FROM imoveis WHERE id="1"';
+    //variavel do tipo array associativo que recebe o resultado da consulta sql.
+    $result = $mysqli->query($img);
+
+    //fecha a conexão
+    $mysqli->close();
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            <!-- place navbar here -->
+        </header>
+        <main>
+            <img src="<?php echo'$img'; ?>" 
+        </main>
+        <footer>
+            <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
