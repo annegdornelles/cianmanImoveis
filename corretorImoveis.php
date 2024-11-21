f1c8a5a383cecc1b929c776eaed29a70745bf71e
amandinha-do-vitin <amandacdenardin@gmail.com>
2024-11-21 09:35:24 -0300

    Update corretorImoveis.php

--- corretorImoveis.php
+++ corretorImoveis.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imóveis</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="src/css/styleCorretorImoveis.css">
+
 </head>
 
 <body>
